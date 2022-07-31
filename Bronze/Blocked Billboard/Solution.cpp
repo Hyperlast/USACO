05cae01a4bf5f71b4e77447740599bcca32b86c6
Hyperlast <72891329+Hyperlast@users.noreply.github.com>
2022-07-31 14:42:35 +0300

    Adding Template to next  task

---
+++ Bronze/Blocked Billboard/Solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <deque>
+#include <utility>
+
+
+void Solution(){
+
+
+
+}
+
+int main(){
+
+
+    Solution();
+    return 0;
+}
